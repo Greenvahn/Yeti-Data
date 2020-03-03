0924a6687d613334582bd9ed3c904c67c6df8fcd
Saul Verde <saulverdemartinez@gmail.com>
2020-03-03 20:07:57 +0000

    Load data file commented in the store

--- src/store/store.js
+++ src/store/store.js
@@ -58,130 +58,141 @@ export const store = new Vuex.Store({
         loadDataFile(state, payload) {
 
 
-                    /* 
-                     FORMAT TABLE VALIDATION
-                     ============================
-                    * If validation check box is active run the following checkings:
-                    * Validate cells --> check if there are empty cells
-                    */
+            /* 
+             FORMAT TABLE VALIDATION
+             ============================
+            * This function is called after the dataArray has been created and validates the format.
+            */
 
-                        // Check if validation is active
-                        const validateFormat = (_data, _options, target) => {
-                            
-                            // isVal false by default
-                            let isVal = false
-                            
-                            // defined maximun items in row
-                            let maxItemsRow = 0;
+            const validateFormat = (_data, _options, target) => {
 
-                            // Iterate throug the options and check if validation is active
-                            _options.forEach(option => {
-                                isVal = option.name === target && option.isCheck ? true : false;
-                            })
+                // isVal false by default
+                let isVal = false
 
-                            // Active validation
+                // defined maximun items in row
+                let maxItemsRow = 0;
 
-                            if (isVal) {
-                                
-                                // Check if empty rows
-                                _data.forEach(row => {
-                                    row.length >= maxItemsRow ?
-                                        maxItemsRow = row.length :
-                                        console.warn('There are empty cells. Max number of cells per row are ' + maxItemsRow);
+                // Iterate throug the parsed options and check if validation is active
+                _options.forEach(option => {
+                    isVal = option.name === target && option.isCheck ? true : false;
+                })
 
-                                    //Activate message 0 -> empty cells
-                                    state.modalStatus.id = 'msg0',
-                                    state.modalStatus.value = true,
-                                    state.modalStatus.params = maxItemsRow
-                                })
+                // If validation is active
+                if (isVal) {
 
-                            } else {
-                                // Validation has not been activated
-                                console.log("%c Validation has NOT been activated ", "color:black; background: orange")
-                            }
+                    // Check if empty rows
+                    _data.forEach(row => {
+                        row.length >= maxItemsRow ?
+                            maxItemsRow = row.length :
+                            console.warn('There are empty cells. Max number of cells per row are ' + maxItemsRow);
 
+                        //Activate message 0 -> empty cells
+                        state.modalStatus.id = 'msg0',
+                        state.modalStatus.value = true,
+                        state.modalStatus.params = maxItemsRow
+                    })
+
+                } else {
+                    // Validation has not been activated
+                    console.log("%c Validation has NOT been activated ", "color:black; background: orange")
+                }
+
+            }
+
+
+            /* 
+             TABLE BUILDER
+             ============================
+            * Checks if the payload contains data.
+            * Check if the file extension is the correct one (csv)
+            * Builds an array with the cells matched by regex:
+                * > Uses commas to divide cells
+                * > Kepps double quotes inside cell
+            */
+
+            // Payload => false
+            if (!payload) {
+                state.dataFile = payload;
+            }
+            // Payload => true
+            else {
+
+                // Retrieves file extension and checks if is correct
+                let _fileExtension = payload.name.split('.').pop().toLowerCase(); // retrieves extension file
+                let extensionSuccess = state.validExtension.indexOf(_fileExtension); // compares agaisnt accepted extensions
+                extensionSuccess != -1 ? extensionSuccess = true : extensionSuccess = false; // returns true if accepted
+
+                // File extesion not accepted
+                if (!extensionSuccess) {
+                    state.modalStatus.id = 'msg1', //Activate message 1 -> format file
+                    state.modalStatus.value = true
+                } 
+                // Extension accepted
+                else {
+
+                    // Creates dataArray to deploy the final data
+                    const dataArray = []
+
+                    // Lets split the data by rows
+                    let rows = payload.result.split("\n");
+
+                    // Filter empty rows or null values
+                    rows = rows.filter(item => {
+                        return (item != null || item != "")
+                    })
+
+
+                    // iterate through the rows
+                    rows.forEach((cell, index) => {
+
+
+                        // make it a string for Regex
+                        let _cell = cell.toString();
+
+                        // Includes commas inside of double quotes
+                        _cell = _cell.match(/(".*?"|[^,]+)(?=\s*,|\s*$)/g);
+
+                        /* will match:
+                            (
+                                ".*?"       double quotes + anything but double quotes + double quotes
+                                |           OR
+                                [^",\s]+    1 or more characters excl. double quotes or comma
+                            )
+                            (?=             FOLLOWED BY
+                                \s*,        0 or more empty spaces and a comma
+                                |           OR
+                                \s*$        0 or more empty spaces and nothing else (end of string)
+                            )
+    
+                        */
+
+                        // Do not push if cell is null
+                        if (_cell != null) {
+                            // push the data cell
+                            dataArray.push(_cell);
                         }
 
 
+                    })
 
 
-            if (!payload) {
-                state.dataFile = payload;
-            } else {
+                    // Validates format table
+                    validateFormat(dataArray, state.inputOptions, 'check data');
 
 
-                //Check if extension file is correct
-                let _fileExtension = payload.name.split('.').pop().toLowerCase();
-                let extensionSuccess = state.validExtension.indexOf(_fileExtension);
-                extensionSuccess != -1 ? extensionSuccess = true : extensionSuccess = false;
 
-                if (!extensionSuccess) {
-                    //Activate message 1 -> format file
-                    state.modalStatus.id = 'msg1',
-                    state.modalStatus.value = true
-                } else {
-                // Retrieves all options
-                const allOptions = state.inputOptions;
-
-                // Creates empty array
-                const dataArray = []
-
-                // Lets split the data by rows
-                let rows = payload.result.split("\n");
-
-                // Filter empty rows or null values
-                rows = rows.filter(item => {
-                    return (item != null || item != "")
-                })
-
-
-                // iterate through the rows
-                rows.forEach((cell, index) => {
-
-
-                    // make it a string for Regex
-                    let _cell = cell.toString();
-
-                    console.log("before" + index, _cell);
-
-                    // Includes commas inside of double quotes
-                    _cell = _cell.match(/(".*?"|[^,]+)(?=\s*,|\s*$)/g);
-
-                    /* will match:
-                        (
-                            ".*?"       double quotes + anything but double quotes + double quotes
-                            |           OR
-                            [^",\s]+    1 or more characters excl. double quotes or comma
-                        )
-                        (?=             FOLLOWED BY
-                            \s*,        0 or more empty spaces and a comma
-                            |           OR
-                            \s*$        0 or more empty spaces and nothing else (end of string)
-                        )
-
+                    /* 
+                    STORE STATE UPDATE
+                    ============================
+                    * mutates the state of the dataFile with dataArray values
                     */
-
-                    // Do not push if cell is null
-                    if (_cell != null) {
-                        // push the data cell
-                        dataArray.push(_cell);
-                    }
-
-
-                })
-
-                // Validates format table
-                validateFormat(dataArray, allOptions, 'check data');
-
-
-                // mutates dataFile state with dataArray values
-                console.table(dataArray);
-                state.dataFile = dataArray;
-
+                    console.table(dataArray);
+                    state.dataFile = dataArray;
 
                 }
 
             }
+            // End of Payload => true
 
         },
         showLaunch(state, payload) {
